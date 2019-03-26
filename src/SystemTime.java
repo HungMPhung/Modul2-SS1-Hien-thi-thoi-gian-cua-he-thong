0d90dae9e138ade333c81f31cd82a969c2e38f76
HungMPhung <minhhhunggtvt2011@gmail.com>
2019-03-26 17:11:13 +0700

    update test int line

--- src/SystemTime.java
+++ src/SystemTime.java
@@ -3,7 +3,7 @@ import java.util.Date;
 public class SystemTime {
     public static void main(String[] args) {
         Date now = new Date();
-
-System.out.println("Now is " + now);
+        System.out.println("Now is " + now);
+        System.out.println("Test");
     }
 }
